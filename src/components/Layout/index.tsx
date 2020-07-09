25ff5f25ffd216026981b38f281ddbdfcb306e98
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-09 14:47:17 -0300

    importing main

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import Main from '../Main';
+import Main from '../Main';
 
 import { Container, Wrapper } from './styles';
 
@@ -9,7 +9,7 @@ const Layout: React.FC = () => {
       <Container>
           <Wrapper>
             {/* <MenuBar /> */}
-            {/* <Main /> */}
+            <Main />
             {/* <SideBar /> */}
           </Wrapper>
       </Container>
