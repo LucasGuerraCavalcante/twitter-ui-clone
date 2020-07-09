f37d5e6fd9f4a026d85ae241c8a825b69908e073
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-09 20:22:45 -0300

    ProfilePage component

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { 
+  Container,
+  Banner,
+  Avatar,
+  ProffileData,
+  LocationIcon,
+  CakeIcon,
+  Followage
+} from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>   
+      <Banner>
+        <Avatar />
+      </Banner>   
+
+      <ProffileData>
+        {/* <EditButton outlined>
+          Edit profile
+        </EditButton> */}
+
+        <h1>Lucas Guerra</h1>
+        <h2>@********</h2>
+
+        <p>
+          Twitter Clone <a href="https://github.com/LucasGuerraCavalcante/twitter-ui-clone">@GitHub</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Brasília, Brazil
+          </li>
+          <li>
+            <CakeIcon />
+            Born May 26, 1999
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            <strong>97 </strong>Following
+          </span>
+          <span>
+            <strong>456 </strong>Followers
+          </span>
+        </Followage>
+      </ProffileData>
+    </Container>
+  );
+}
+
+export default Main;
