7ce604059413eb752b774eddb5519bd4f26f1548
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-11 20:43:25 -0300

    importing tweet

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { Container, Tab, Tweets } from './styles';
 
+import Tweet from '../Tweet';
+
 const Feed: React.FC = () => {
   return (
       <Container>
           <Tab>Tweets</Tab>
 
           <Tweets>
-              {/* <Tweet />
               <Tweet />
               <Tweet />
               <Tweet />
@@ -18,7 +19,8 @@ const Feed: React.FC = () => {
               <Tweet />
               <Tweet />
               <Tweet />
-              <Tweet /> */}
+              <Tweet />
+              <Tweet />
           </Tweets> 
       </Container>
   );
