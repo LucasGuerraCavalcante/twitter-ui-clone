5a38cf91b20d9980ec7001b1908a94b5fe85e14c
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-23 15:26:32 -0300

    MenuBar bot side

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { 
     Container,
     Topside,
+    Botside,
+    Avatar,
+    ProfileDate,
     Logo,
     MenuButton,
     HomeIcon,
@@ -12,7 +15,8 @@ import {
     ProfileIcon,
     HashIcon,
     ListIcon,
-    MoreIcon
+    MoreIcon,
+    ExitIcon
 } from './styles';
 
 import Button from '../Button';
@@ -20,53 +24,64 @@ import Button from '../Button';
 const MenuBar: React.FC = () => {
   return (
       <Container>
-          <Topside>
-              <Logo />
+        <Topside>
+            <Logo />
 
-              <MenuButton>
-                  <HomeIcon />
-                  <span>Home</span>
-              </MenuButton>
+            <MenuButton>
+                <HomeIcon />
+                <span>Home</span>
+            </MenuButton>
 
-              <MenuButton>
-                  <HashIcon />
-                  <span>Explore</span>
-              </MenuButton>
+            <MenuButton>
+                <HashIcon />
+                <span>Explore</span>
+            </MenuButton>
 
-              <MenuButton>
-                  <BellIcon />
-                  <span>Notifications</span>
-              </MenuButton>
+            <MenuButton>
+                <BellIcon />
+                <span>Notifications</span>
+            </MenuButton>
 
-              <MenuButton>
-                  <EmailIcon />
-                  <span>Messages</span>
-              </MenuButton>
+            <MenuButton>
+                <EmailIcon />
+                <span>Messages</span>
+            </MenuButton>
 
-              <MenuButton>
-                  <FavoriteIcon />
-                  <span>Bookmarks</span>
-              </MenuButton>
+            <MenuButton>
+                <FavoriteIcon />
+                <span>Bookmarks</span>
+            </MenuButton>
 
-              <MenuButton>
-                  <ListIcon />
-                  <span>Lists</span>
-              </MenuButton>
+            <MenuButton>
+                <ListIcon />
+                <span>Lists</span>
+            </MenuButton>
 
-              <MenuButton className="active">
-                  <ProfileIcon />
-                  <span>Profile</span>
-              </MenuButton>
+            <MenuButton className="active">
+                <ProfileIcon />
+                <span>Profile</span>
+            </MenuButton>
 
-              <MenuButton>
-                  <MoreIcon />
-                  <span>More</span>
-              </MenuButton>
+            <MenuButton>
+                <MoreIcon />
+                <span>More</span>
+            </MenuButton>
 
-              <Button>
-                  <span>Tweet</span>
-              </Button>
-          </Topside>
+            <Button>
+                <span>Tweet</span>
+            </Button>
+        </Topside>
+
+        <Botside>
+            <Avatar />
+
+            <ProfileDate>
+                <strong>Lucas Guerra</strong>
+                <span>@********</span>
+            </ProfileDate>
+
+            <ExitIcon />
+        </Botside>
       </Container>
   );
 }
--- src/components/MenuBar/styles.ts
+++ src/components/MenuBar/styles.ts
@@ -42,6 +42,40 @@ export const Topside = styled.div`
   }
 `;
 
+export const Botside = styled.div`
+  margin-top: 20px;
+
+  display: flex;
+  align-items: center;
+`
+
+export const Avatar = styled.div`
+  width: 39px;
+  height: 39px;
+
+  flex-shrink: 0;
+
+  border-radius: 50%;
+  background: var(--gray);
+`
+
+export const ProfileDate = styled.div`
+  display: none;
+
+  @media (min-width: 1280px) {
+    display: flex;
+    flex-direction: column;
+
+    margin-left: 10px;
+    font-size: 14px;
+
+    > span {
+      color: var(--gray);
+    }
+
+  }
+`
+
 export const MenuButton = styled.button`
   display: flex;
   align-items: center;
@@ -155,3 +189,25 @@ export const MoreIcon = styled(More)`
 `;
 
 
+
+export const ExitIcon = styled(ExitToApp)`
+  display: none;
+
+  @media (min-width: 1280px) {
+    display: inline-block;
+
+    width: 25px;
+    height: 25px;
+
+    color: var(--white);
+
+    margin-left: 30px;
+    cursor: pointer;
+
+    &:hover {
+      > path {
+        color: var(--like)
+      }
+    }
+  }
+`;
